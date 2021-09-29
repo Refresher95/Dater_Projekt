7ae9285970cdc475add779c07507b71e8a5f0949
Refresher95 <74629396+Refresher95@users.noreply.github.com>
2021-09-30 00:48:21 +0200

    Delete Dater__Projekt directory

--- Dater__Projekt/benutzer_table.sql
+++
@@ -1,3 +0,0 @@
-use dater_benutzer_datensätze;
-select * from benutzer_table;
--- SELECT Benutzer_Vorname,Benutzer_Nachname FROM benutzer_table WHERE Benutzer_Nutzername = 'kd' AND Benutzer_Passwort = 'dk';benutzer_tableBenutzer_Geburtstagsdatum
